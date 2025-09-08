331436e3557edeb81c4c6c255fcf6bbe7a111ebf
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-09-08 18:26:02 -0500

    Delete pages directory

--- pages/indicadores.jsx
+++
@@ -1,2 +0,0 @@
-import PageEditor from './_pageEditor'
-export default function Indicadores(){ return <PageEditor slug='indicadores' title='Indicadores' defaultContent={[{ id:'i1', value:'<h2>Indicadores</h2><p>Contenido editable</p>', x:20, y:20, width:320 }]} /> }
--- pages/metas.jsx
+++
@@ -1,2 +0,0 @@
-import PageEditor from './_pageEditor'
-export default function Metas(){ return <PageEditor slug='metas' title='Metas' defaultContent={[{ id:'m1', value:'<h2>Metas</h2><p>Contenido editable</p>', x:20, y:20, width:320 }]} /> }
