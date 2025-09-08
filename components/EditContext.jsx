d129ac1a21babe4eacc71d253f1f7a319a097f6a
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-09-08 18:25:45 -0500

    Delete components directory

--- components/EditContext.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext, useState } from 'react'
-const EditContext = createContext()
-export function EditProvider({ children }){
-  const [editMode, setEditMode] = useState(false)
-  return <EditContext.Provider value={{ editMode, setEditMode }}>{children}</EditContext.Provider>
-}
-export function useEdit(){ return useContext(EditContext) }
