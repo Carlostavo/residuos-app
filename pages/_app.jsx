e24a15fb19d3ca78605772df66022a808613c050
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-09-09 21:41:17 -0500

    Delete pages directory

--- pages/_app.jsx
+++
@@ -1,4 +0,0 @@
-import '../styles/globals.css'
-import Nav from '../components/Nav'
-import { EditProvider } from '../components/EditContext'
-export default function MyApp({ Component, pageProps }){ return (<EditProvider><Nav /><Component {...pageProps} /></EditProvider>) }
--- pages/_pageEditor.jsx
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react'
-import EditorShell from '../components/EditorShell'
-export default function PageEditor({ slug='home' }){
-  const [blocks, setBlocks] = useState([{ id:'b1', type:'text', value:'Bienvenido', x:40, y:40, width:360 }])
-  async function save(){ alert('Guardar implementa supabase upsert') }
-  return <EditorShell blocks={blocks} setBlocks={setBlocks} onSave={save} />
-}
