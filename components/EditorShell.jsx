7d4f9ba3c5b92e341829ec8e64c4fcb5b655dfed
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-09-10 10:34:09 -0500

    Update EditorShell.jsx

--- components/EditorShell.jsx
+++ components/EditorShell.jsx
@@ -1,29 +1,14 @@
-import React from 'react'
-import EditorSidebar from './EditorSidebar'
-import CanvasBlock from './CanvasBlock'
 import ToolbarTop from './ToolbarTop'
-import { useEdit } from './EditContext'
+import EditorSidebar from './EditorSidebar'
 
-export default function EditorShell({ blocks, setBlocks, pageId, onSave, onPreview, onHistory }){
-  const { editMode, showSidebar } = useEdit()
-
-  function updateBlock(id, html){ setBlocks(prev=> prev.map(b=> b.id===id? {...b, value: html} : b)) }
-  function onUpdatePos(id, pos){ setBlocks(prev=> prev.map(b=> b.id===id? {...b, ...pos} : b)) }
-  function onSelect(id){ /* leave selection to sidebar */ }
-
-  const ordered = [...blocks].sort((a,b)=> (a.zIndex||0) - (b.zIndex||0))
-
+export default function EditorShell({ children, onSave, onUndo, onRedo, onPreview, onExit }) {
   return (
-    <div>
-      <ToolbarTop onSave={onSave} onPreview={onPreview} onHistory={onHistory} onUndo={()=>{}} onRedo={()=>{}} editMode={editMode} />
-      <div style={{ height: 'calc(100vh - 120px)' }} className={showSidebar? 'canvas-offset' : ''}>
-        <div className="canvas-fullscreen">
-          <div className="canvas-inner">
-            {ordered.map(b=> <CanvasBlock key={b.id} block={b} editMode={editMode} onChange={updateBlock} onUpdatePos={onUpdatePos} onSelect={onSelect} selected={false} />)}
-          </div>
-        </div>
+    <div className="h-screen">
+      <ToolbarTop onSave={onSave} onUndo={onUndo} onRedo={onRedo} onPreview={onPreview} onExit={onExit} />
+      <EditorSidebar />
+      <div className="ml-80 mt-14 h-[calc(100%-3.5rem)] overflow-auto bg-gray-50 p-6">
+        {children}
       </div>
-      <EditorSidebar blocks={blocks} setBlocks={setBlocks} />
     </div>
   )
 }
