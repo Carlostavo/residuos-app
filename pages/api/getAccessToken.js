693234727ba30adc8892a21a834eb5fb056e2d00
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-09-17 21:47:35 -0500

    Delete pages directory

--- pages/api/getAccessToken.js
+++
@@ -1,6 +0,0 @@
-import { supabase } from '../../lib/supabaseClient'
-export default async function handler(req, res){
-  // On the client, call supabase.auth.getSession() to get access_token properly.
-  // This endpoint is just a placeholder and returns 400.
-  res.status(400).send('Use client-side supabase.auth.getSession() to obtain token')
-}
