643156be686f423f91bd9f1f9854bc6311d8fa40
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-09-18 12:55:30 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,4 @@
 import './globals.css'
-
-
 import Header from '../components/Header'
 
 export const metadata = {
@@ -11,6 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+      </head>
       <body className="bg-gray-50 min-h-screen">
         <Header />
         <main className="p-6">{children}</main>
